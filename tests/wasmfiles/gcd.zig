c9a0b26e15c7b70d7c54a3fe74b6616553e9199f
youdi323323 <kouki32312171217@gmail.com>
2026-01-18 12:56:19 +0900

    The codebase is too big...

--- tests/wasmfiles/gcd.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-
-export fn gcd(a: u32, b: u32) callconv(.C) u32 {
-    return if (b != 0) gcd(b, @mod(a, b)) else a;
-}
-
-pub fn main() void {
-    _ = std.c.printf("Hello, world! Please call gcd(10, 5) to see the result.\n");
-}
