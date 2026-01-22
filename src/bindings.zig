8a837afe29d3fb65032e8a95d194722072dfd06a
youdi323323 <kouki32312171217@gmail.com>
2026-01-23 00:50:54 +0900

    brub

--- src/bindings.zig
+++ src/bindings.zig
@@ -1,3 +1,3 @@
-pub const wasm_export = @import("wasm_export");
-pub const wasm_c_api = @import("wasm_c_api");
-pub const bh_read_file = @import("bh_read_file");
+pub const wasm_export = @cImport("wasm_export");
+pub const wasm_c_api = @cImport("wasm_c_api");
+pub const bh_read_file = @cImport("bh_read_file");
