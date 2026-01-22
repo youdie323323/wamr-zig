1e48d053fa7f4a7b7b2b00f03ca0fd720bc92ca8
youdi323323 <kouki32312171217@gmail.com>
2026-01-23 00:57:56 +0900

    brub

--- src/bindings.zig
+++ src/bindings.zig
@@ -1,3 +1,20 @@
-pub const wasm_export = @cImport("wasm_export");
-pub const wasm_c_api = @cImport("wasm_c_api");
-pub const bh_read_file = @cImport("bh_read_file");
+pub const wasm_export = @cImport({
+    @cDefine("WASM_ENABLE_INTERP", "1");
+    @cDefine("WASM_ENABLE_AOT", "0");
+
+    @cInclude("wamr/core/iwasm/include/wasm_export.h");
+});
+pub const wasm_c_api = @cImport({
+    @cDefine("WASM_ENABLE_INTERP", "1");
+    @cDefine("WASM_ENABLE_AOT", "0");
+
+    @cInclude("wamr/core/iwasm/include/wasm_c_api.h");
+});
+pub const bh_read_file = @cImport({
+    if (builtin.os.tag == .windows)
+        @cDefine("WIN32_LEAN_AND_MEAN", "1");
+
+    @cInclude("wamr/core/shared/utils/uncommon/bh_read_file.h");
+});
+
+const builtin = @import("builtin");
