c9e80361a65320054cf9cecbdd2d2d52840b884a
youdi323323 <kouki32312171217@gmail.com>
2026-01-26 02:52:23 +0900

    brub

--- build.zig
+++ build.zig
@@ -55,6 +55,7 @@ pub fn build(b: *std.Build) !void {
 
     const cmake_build_type = switch (optimize) {
         .Debug => "Debug",
+
         else => "MinSizeRel",
     };
 
@@ -86,10 +87,6 @@ pub fn build(b: *std.Build) !void {
         wamr_module.linkSystemLibrary("bcrypt", .{});
         wamr_module.linkSystemLibrary("userenv", .{});
         wamr_module.linkSystemLibrary("advapi32", .{});
-
-        wamr_module.linkSystemLibrary("ucrt", .{});
-        wamr_module.linkSystemLibrary("vcruntime", .{});
-        wamr_module.linkSystemLibrary("msvcrt", .{});
     } else wamr_module.addLibraryPath(b.path(".zig-cache"));
 
     wamr_module.linkSystemLibrary("iwasm", .{ .use_pkg_config = .no });
@@ -118,9 +115,9 @@ fn buildCMake(
 
     if (target.result.os.tag == .windows) {
         if (std.mem.eql(u8, build_type, "Debug"))
-            cmake_config.addArg("-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebug")
+            cmake_config.addArg("-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebugDLL")
         else
-            cmake_config.addArg("-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded");
+            cmake_config.addArg("-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL");
 
         cmake_config.addArg("-DCMAKE_C_FLAGS=/FS /std:c11 /Dalignof=__alignof /Dstatic_assert=_Static_assert /D__attribute__(x)=");
         cmake_config.addArg("-DCMAKE_CXX_FLAGS=/FS");
