5dceab1877b283b073278fc64da24a5e55cd898d
youdi323323 <kouki32312171217@gmail.com>
2026-03-09 10:27:57 +0900

    mood

--- build.zig
+++ build.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const mem = std.mem;
 const process = std.process;
 const log = std.log;
+const Thread = std.Thread;
 
 const builtin = @import("builtin");
 
@@ -81,15 +82,15 @@ pub fn build(b: *std.Build) !void {
         wamr_mod.addLibraryPath(b.path(b.fmt(".zig-cache/{s}", .{cmake_build_type})));
 
         if (target.result.os.tag == .windows) {
-            // { // Add libraries (requires running on 'x64 Native Tools Command Prompt')
-            //     const lib = try process.getEnvVarOwned(b.allocator, "LIB");
-            //     defer b.allocator.free(lib);
+            { // Add libraries (requires running on 'x64 Native Tools Command Prompt')
+                const lib = try process.getEnvVarOwned(b.allocator, "LIB");
+                defer b.allocator.free(lib);
 
-            //     var it = mem.tokenizeScalar(u8, lib, ';');
-            //     while (it.next()) |path|
-            //         if (path.len > 0)
-            //             wamr_mod.addLibraryPath(.{ .cwd_relative = b.allocator.dupe(u8, path) catch unreachable });
-            // }
+                var it = mem.tokenizeScalar(u8, lib, ';');
+                while (it.next()) |path|
+                    if (path.len > 0)
+                        wamr_mod.addLibraryPath(.{ .cwd_relative = b.allocator.dupe(u8, path) catch unreachable });
+            }
 
             wamr_mod.linkSystemLibrary("uuid", .{});
             wamr_mod.linkSystemLibrary("pathcch", .{});
@@ -138,7 +139,7 @@ fn buildCMake(
     cmake_config.addArg("-DBUILD_SHARED_LIBS=OFF");
 
     if (target.result.os.tag == .windows) {
-        if (std.mem.eql(u8, build_type, "Debug"))
+        if (mem.eql(u8, build_type, "Debug"))
             cmake_config.addArg("-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebugDLL")
         else
             cmake_config.addArg("-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL");
@@ -150,7 +151,7 @@ fn buildCMake(
     cmake_config.addPrefixedDirectoryArg("-S", root);
     cmake_config.addPrefixedDirectoryArg("-B", cache_path);
 
-    const cpu_count = std.Thread.getCpuCount() catch 1;
+    const cpu_count = Thread.getCpuCount() catch 1;
 
     const cmake_build = b.addSystemCommand(&.{"cmake"});
 
